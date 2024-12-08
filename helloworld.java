33a0b478bc7532a0dfa197e44b83a7dcb5ff67ba
CHANDH1516 <22h51a0546@cmrcet.ac.in>
2024-12-09 04:58:28 +0530

    Update D

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 // Your First Program
 
-class helloworld {
+class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("HI, World!"); 
     }
 }
